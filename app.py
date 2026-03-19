b3f269a46dbcfcf2b059d492e9e00b5335a83d59
23kq1a05f0-lab <23kq1a05f0@pace.ac.in>
2026-03-19 21:02:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+st.write("NEW VERSION LOADED ✅")
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
